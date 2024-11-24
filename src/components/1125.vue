9ed5af44cb944b51b8f17ef77ec36840252ed181
JustMonika <2038577184@qq.com>
2024-11-24 17:43:50 +0800

    update

--- src/components/1125.vue
+++ src/components/1125.vue
@@ -9,6 +9,7 @@
   <div @mousedown.stop="startDrag('mymusic', $event)" @touchstart.stop="startTouchDrag('mymusic', $event)" class="window"
     :class="{ 'closing': windowClosingStatus.mymusic }"
     :style="{ top: `${position.mymusic.y}px`, left: `${position.mymusic.x}px` }" id="mymusic">
+
     <div class="title-bar">
       <div class="title-bar-text">A Music player</div>
       <div class="title-bar-controls">
@@ -34,6 +35,10 @@
     <div class="window-body" id="lainsbody">
       <img src="/assets/16c.gif" alt="G O D" style="margin-top: 30px;width: 630px; height: auto;">
     </div>
+  </div>
+  <div id="buttonstart">
+    <div><button @click="showWindow('mymusic')">A Music player</button></div>
+    <div><button @click="showWindow('lain')">Just Lain 'GOD'</button></div>
   </div>
 </template>
 
@@ -134,7 +139,14 @@ const windowClosingStatus = ref({
   mymusic: false,
   lain: false
 });
-
+function showWindow(windowId: string) {
+  // 如果窗口已经是打开状态，则不做任何操作
+  if (windowClosingStatus.value[windowId]) {
+    windowClosingStatus.value[windowId] = false; // 重置关闭状态
+    console.log([windowId] + "被打开了");
+    // 可以在这里添加额外的逻辑，比如重置窗口位置等
+  }
+}
 // 关闭窗口的函数，接收窗口ID作为参数
 function closeWindow(windowId: string) {
   windowClosingStatus.value[windowId] = true;
@@ -150,12 +162,6 @@ function closeWindow(windowId: string) {
     // isClosing.value[windowId] = false; // 这通常会在重新打开窗口的逻辑中处理
 
     console.log(`${windowId} 已关闭`);
-
-    // 注意：由于我们使用了v-if或v-show来控制窗口的显示，
-    // 因此实际上不需要在这里将窗口的display属性设置为none。
-    // Vue会根据v-if或v-show的值来自动处理DOM的添加和移除或显示和隐藏。
-    // 如果您想要在页面上完全移除窗口元素，可以在父组件中控制一个变量，
-    // 该变量决定是否渲染该窗口组件，并使用v-if来绑定这个变量。
   }, 200); // 延迟200毫秒模拟渐隐效果
 }
 
@@ -163,24 +169,24 @@ const playOrder = ref<playOrderType>('along');
 type playOrderType = 'along' | 'random' | 'loop';
 const musicList = ref<musicListType>([
   {
-    name: '僕らの手には何もないけど、',
-    author: 'RAM WIRE',
-    url: '/assets/Vite App.mp3',
-    img: '/assets/RAM WIRE.jpg',
+    name: 'NOTOK',
+    author: 'ヒトリエ',
+    url: 'https://m704.music.126.net/20241124172542/7631a644268f4f4d70d53bda0ff89f1e/jdyyaac/obj/w5rDlsOJwrLDjj7CmsOj/56064154066/2b9f/c175/6272/030808d00c5e9f3de64d42ce0b1e8cb9.m4a?authSecret=000001935d66daf31e130a32fb7f0006',
+    img: 'http://p2.music.126.net/V89JpcVkGSSZHFpZmZlqzQ==/109951170088068396.jpg?param=130y130',
     lrc: ``
   },
   {
-    name: '啊啊啊，灵梦你在这里干什么啊',
-    author: '9',
-    url: '/assets/啊啊啊，灵梦你在这里干什么啊.wav',
-    img: '/assets/1.png',
+    name: 'Duvet',
+    author: 'Bôa',
+    url: 'https://m704.music.126.net/20241124173014/612ef4dcff911f9c4633e70bd3a1490f/jdyyaac/obj/w5rDlsOJwrLDjj7CmsOj/11704850417/3c3b/6d8b/0aaf/cb6ead75e2d5511de35c315c6002bcf6.m4a?authSecret=000001935d6b01f205d10a32d78d0006',
+    img: '/assets/16c.gif',
     lrc: ``
   },
   {
-    name: '我没有帮饲养员抢到bw的票啊',
+    name: '悠久を漂流う,Magical Astronomy',
     author: 'Reimu ',
-    url: '/assets/我没有帮饲养员抢到bw的票啊.wav',
-    img: '/assets/1.png',
+    url: 'https://m10.music.126.net/20241124173442/9de7ff4b66fadac8b2b61603858ba57d/yyaac/5453/025f/0152/c95388366755c2fc3b8c68220bab7990.m4a',
+    img: 'http://p2.music.126.net/-_u6apF2XSR870RJ1xMIgw==/908196604586021.jpg?param=130y130',
     lrc: ``
   },
 
@@ -215,6 +221,13 @@ const musicList = ref<musicListType>([
   image-rendering: pixelated;
   background-size: auto;
 }
+
+#buttonstart {
+  display: flex;
+  justify-content: flex-end;
+}
+
+
 
 .d {
   /* margin: auto; */
